a2ed12974530f96a270112f1ce8aa8cadd0859c2
Laborde Romain <r.laborde@etud.univ-pau.fr>
2023-06-05 18:37:08 +0200

    ajout dossier conception

---
+++ v7_lecteurvue_BD/lecteur_modele.cpp
@@ -0,0 +1,84 @@
+#include "lecteur_modele.h"
+#include <QSqlQuery>
+#include <QSqlRecord>
+#include <QSqlField>
+
+LecteurModele::LecteurModele()
+{
+    _dataBase = new Database;
+    Diaporama _diaporama;
+}
+
+void LecteurModele::avancer()
+{
+    _diaporama.avancerImageCourante();
+}
+
+void LecteurModele::reculer()
+{
+    _diaporama.reculerImageCourante();
+}
+
+void LecteurModele::changerDiaporama(unsigned int pNumDiaporama)
+{
+    // s'il y a un diaporama courant, le vider, puis charger le nouveau Diaporama
+    if (_numDiaporamaCourant > 0)
+    {
+        _diaporama.viderDiaporama();
+    }
+    _numDiaporamaCourant = pNumDiaporama;
+    if (_numDiaporamaCourant > 0)
+    {
+
+        chargerDiaporama(); // charge le diaporama courant
+    }
+
+}
+
+QStringList LecteurModele::getNomDiapo()
+{
+    QSqlQuery query;
+    QStringList nomsDiapo;
+    query.exec("Select * From Diaporamas");
+    for (int i=0; query.next(); i++)
+    {
+        qDebug() << query.value(1).toString();
+        nomsDiapo.append(query.value(1).toString());
+    }
+    return nomsDiapo;
+}
+
+void LecteurModele::chargerDiaporama()
+{
+    //Diaporama _diaporama(_numDiaporamaCourant, _nomDiaporamaCourant);
+    QSqlQuery query;
+    query.prepare("SELECT DDD.rang, Ds.titrePhoto, Ds.uriPhoto, F.nomFamille FROM DiaposDansDiaporama DDD JOIN Diaporamas Dps ON DDD.idDiaporama = Dps.idDiaporama JOIN Diapos Ds ON DDD.idDiapo = Ds.idphoto JOIN Familles F ON Ds.idFam = F.idFamille WHERE DDD.idDiaporama = :id");
+    query.bindValue(":id", _numDiaporamaCourant);
+    query.exec();
+    for (int i = 0; query.next(); i++) {
+        _diaporama.ajouterImage(query.value(0).toInt(),
+                                query.value(3).toString().toStdString(),
+                                query.value(1).toString().toStdString(),
+                                ":"+query.value(2).toString().toStdString());
+    }
+
+     // trie du contenu du diaporama par ordre croissant selon le rang de l'image dans le diaporama
+    _diaporama.triDiaporama();
+
+
+}
+
+unsigned int LecteurModele::nbImages()
+{
+    return _diaporama.nbImages();
+}
+
+Image *LecteurModele::imageCourante()
+{
+    return _diaporama.imageCourante();
+}
+
+unsigned int LecteurModele::numDiaporamaCourant()
+{
+    return _numDiaporamaCourant;
+}
